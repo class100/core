0c371acc5cfe52a1838a2fdbe61fe1989e11ba6e
i9code <48942204+i9code@users.noreply.github.com>
2021-01-20 17:02:52 +0800

    YUNKE-2576 【Server】增加环境配置（开发、本地、生产、测试）

--- environment.go
+++ environment.go
@@ -1,18 +1,18 @@
 package core
 
 const (
-	// EnvironmentTypeDev 开发
-	EnvironmentTypeDev Environment = "dev"
-	// EnvironmentTypeTest 测试
-	EnvironmentTypeTest Environment = "test"
-	// EnvironmentTypeQa 测试
-	EnvironmentTypeQa Environment = "qa"
-	// EnvironmentTypeProd 生产
-	EnvironmentTypeProd Environment = "prod"
-	// EnvironmentTypeLocal 本地环境
-	EnvironmentTypeLocal Environment = "local"
-	// EnvironmentTypeSimulation 模拟请求（不发真实请求到服务器）
-	EnvironmentTypeSimulation Environment = "simulation"
+	// EnvironmentDev 开发
+	EnvironmentDev Environment = "dev"
+	// EnvironmentTest 测试
+	EnvironmentTest Environment = "test"
+	// EnvironmentQa 测试
+	EnvironmentQa Environment = "qa"
+	// EnvironmentProd 生产
+	EnvironmentProd Environment = "prod"
+	// EnvironmentLocal 本地环境
+	EnvironmentLocal Environment = "local"
+	// EnvironmentSimulation 模拟请求（不发真实请求到服务器）
+	EnvironmentSimulation Environment = "simulation"
 )
 
 // Environment 环境类型
